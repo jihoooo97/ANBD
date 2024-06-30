89b647c4e9921358716d8acc0fd9d2aae667b2e1
Jiho <yjh7827@naver.com>
2024-07-01 00:02:42 +0900

    feat: DI 기본 세팅

--- Projects/App/Sources/ANBDApp.swift
+++ Projects/App/Sources/ANBDApp.swift
@@ -1,12 +1,17 @@
-import ANBDCore
-import Presentation
+import CommonUI
 import SwiftUI
+import Swinject
 
 @main
 struct ANBDApp: App {
-
+    private let injector: Injector
+    
     init() {
-        
+        self.injector = DependencyInjector(container: .init())
+        injector.assemble([
+            CoreAssembly(),
+            PresentationAssembly()
+        ])
     }
 
     var body: some Scene {
---
+++ Projects/App/Sources/DI/PresentationAssembly.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationAssembly.swift
+//  ANBD
+//
+//  Created by 유지호 on 6/30/24.
+//  Copyright © 2024 jiho. All rights reserved.
+//
+
+import Swinject
+
+final class PresentationAssembly: Assembly {
+    
+    func assemble(container: Container) {
+        
+    }
+    
+}
