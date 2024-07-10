400b8e75177a955d690587a702f1b3cbcc11d1d6
Jiho <yjh7827@naver.com>
2024-07-11 02:13:24 +0900

    fix: Firebase 세팅 코드 제거

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -15,19 +15,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
-//        let secondaryOptions = FirebaseOptions(
-//            googleAppID: "1:1059046414714:ios:9856770633e4a28fa7dec3",
-//            gcmSenderID: "1059046414714"
-//        )
-//        
-//        secondaryOptions.apiKey = "AIzaSyDnl-46vzGqqXw8CMZhi2LWClinfimQTO4"
-//        secondaryOptions.projectID = "anbd-project3"
-//        secondaryOptions.bundleID = "com.jiho.ANBD"
-//        secondaryOptions.clientID = "1059046414714-rj1vpip9i5641dnp55slddsrpee2ab6m.apps.googleusercontent.com"
-//        secondaryOptions.storageBucket = "anbd-project3.appspot.com"
-//        secondaryOptions.appGroupID = nil
-//        
-//        FirebaseApp.configure(options: secondaryOptions)
         FirebaseApp.configure()
         return true
     }
